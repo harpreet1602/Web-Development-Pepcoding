9d210f573500c9b8d917be33261cc03e0b07647e
harpreet1602 <harpreets1602@gmail.com>
2021-06-24 21:30:40 +0530

    update

--- Excel/script.js
+++ Excel/script.js
@@ -3,6 +3,15 @@ body.spellcheck = false;
 
 let menuBarPtags = document.querySelectorAll(".menu-bar p");
 
+let columnTags=document.querySelector(".column-tags");
+
+let rowNumbers =  document.querySelector(".row-numbers");
+
+let grid = document.querySelector(".grid");
+
+let oldCell=undefined;
+
+let formulaSelectCell = document.querySelector("select-cell");
 //here I need to highlight the option selected and also remove the highlight on clicking it or any other
 // option in menu bar 
 
@@ -23,4 +32,56 @@ for(let i=0;i<menuBarPtags.length;i++)
             e.currentTarget.classList.add("menu-bar-option-selected");
         }
     });
+}
+
+for(let i=0;i<26;i++)
+{
+    let div = document.createElement("div");
+    div.classList.add("column-tag-cell");
+    div.innerText = String.fromCharCode(65+i);
+    columnTags.append(div);
+}
+
+
+for(let i=1;i<=100;i++)
+{
+    let div = document.createElement("div");
+    div.classList.add("row-number-cell");
+    div.innerText = i;
+    rowNumbers.append(div);
+}
+
+for(let j=1;j<=100;j++)
+{
+let row=document.createElement("div");
+row.classList.add("row");
+//j=1 
+//i=0; i+65 => 65 (A)  ==> A1
+//i=1; i+65 => 66(B)   ==>B1
+
+//                        Z1
+//j=2 repeat
+for(let i=0;i<26;i++)
+{
+    let cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.setAttribute("data-address",Sting.fromCharCode(i+65)+j);
+
+    cell.addEventListener("click",function(e){
+        if(oldCell)
+        {
+            oldCell.classList.remove("grid-selected-cell");
+        }
+        e.currentTarget.classList.add("grid-selected-cell");
+
+        let cellAddress = e.currentTarget.getAttribute("data-address");
+        
+
+
+    });
+
+    cell.contentEditable=true;
+    row.append(cell);
+}
+grid.append(row);
 }
