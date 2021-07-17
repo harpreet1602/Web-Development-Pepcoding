6746cccf26b4e50cd03238b8a9e72d0e9f6e4454
harpreet1602 <harpreets1602@gmail.com>
2021-07-17 22:27:45 +0530

    update

---
+++ React/testProjectBackend/server.js
@@ -0,0 +1,11 @@
+let express = require("express");
+
+let data = require("./data.json");
+
+let server = express();
+
+server.get("/pets",function(req,res){
+    res.json(data);
+});
+
+server.listen(4000);
